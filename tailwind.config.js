c8300a1e7b5e3e6793e05d218b2dbe2d347e4048
EpicEladin <etien.elersic@gmail.com>
2026-05-01 15:53:02 +0200

    Frist Propper

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-main': '#0B5778',
+        'light-main': '#48A9A6',
+
+        'light-acc': '#1b0e27',
+        'dark-acc': '#f8f4e3',
+
+        'light-page': '#F3F3F1',
+        'dark-page': '#594F60',
+
+        'brand': '#ff6b35',
+        'accent': {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          500: '#0ea5e9',
+          900: '#0c2d6b',
+        }
+      }
+    }
+  }
+}
